422754a61cdfaa8c26698b058fe7c32a721a3b8c
Sasha Morozov <aa.morozov@outlook.com>
2023-11-21 15:12:33 +0100

    Initial Commit

---
+++ Budgeting-App/Budgeting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budgeting_AppApp.swift
+//  Budgeting-App
+//
+//  Created by Aleksandr Morozov on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Budgeting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgeting-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgeting-App
+//
+//  Created by Aleksandr Morozov on 21/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
